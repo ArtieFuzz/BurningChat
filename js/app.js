af494a7bb625334641f9b856a17f7d9b79b9b25a
HyeonuPark <nemo1275@gmail.com>
2014-11-19 18:31:52 +0900

    for public!

--- js/app.js
+++ js/app.js
@@ -1,43 +1,71 @@
 var config = {
     server : 'https://burningchat.firebaseio.com',
     colors : [
-        'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'
+        'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal'
     ]
-}
+};
 
-var app = angular.module('app', ['firebase']);
+var app = angular.module('app', ['firebase', 'ngCookies', 'luegg.directives', 'burningForeignLanguage']);
 
 app.value('firebase', new Firebase(config.server));
 
-app.controller('controller', function($scope, $firebase, firebase) {
+app.value('bflConfig', {
+    baseUrl : '/bfl/',
+    defaultLang : 'en_US',
+    langs : [
+        'en_US',
+        'ko_KR'
+    ]
+});
+
+app.controller('controller', function($scope, $firebase, $cookies, firebase, bflLanguage) {
+
+    $scope.langs = bflLanguage.langs;
+    $scope.setLang = bflLanguage.setLang;
 
     var $msgRef = $firebase(firebase.child('msg'));
     var $userRef = $firebase(firebase.child('user'));
-    var color = config.colors[Math.floor(Math.random() * config.colors.length)];
 
     var me = {
-        nick : 'John Smith',
+        nick : $cookies.burningchat_nick || 'John Smith',
         text : '',
         style: {
-            color : color
+            color : $cookies.burningchat_color || config.colors[Math.floor(Math.random() * config.colors.length)]
         }
+    };
+    $scope.me = me;
+
+    if ($cookies.burningchat_id) {
+        $userRef.$set($cookies.burningchat_id, me).then(function(myRef) {
+            myRef.onDisconnect().remove();
+            $firebase(myRef).$asObject().$bindTo($scope, 'me');
+        });
+    } else {
+        $userRef.$push(me).then(function(myRef) {
+            $cookies.burningchat_id = myRef.key();
+            myRef.onDisconnect().remove();
+            $firebase(myRef).$asObject().$bindTo($scope, 'me');
+        });
     }
-    $userRef.$push(me).then(function(myRef) {
-        myRef.onDisconnect().remove();
-        $firebase(myRef).$asObject().$bindTo($scope, 'me');
-    });
+
+    $scope.$watch('me', function() {
+        $cookies.burningchat_nick = $scope.me.nick;
+        $cookies.burningchat_color = $scope.me.style.color;
+    }, true);
 
     $scope.lines = $msgRef.$asArray();
     $scope.users = $userRef.$asArray();
 
     $scope.send = function() {
+        if (!$scope.me.text) return;
         $msgRef.$push({
+            type : 'msg',
             time : new Date().getTime(),
             nick : $scope.me.nick,
             msg : $scope.me.text
         });
         $scope.me.text = '';
-    }
+    };
 
     $scope.locale = function(time) {
         return moment.utc(time).local().format($scope.timestamp);
---
+++ js/burningForeignLanguage.js
@@ -0,0 +1,54 @@
+(function(angular){
+    'use strict';
+
+    angular.module('burningForeignLanguage', [])
+        .factory('bflLanguage', function($http, bflConfig) {
+            var result = {
+                langs : bflConfig.langs,
+                defaultLang : bflConfig.defaultLang,
+                lang : bflConfig.defaultLang,
+                dictionary : [],
+                prvDict : [],
+                defaultDict : []
+            };
+            $http.get(bflConfig.baseUrl + result.defaultLang).success(function(data) {
+                result.defaultDict = data;
+                result.dictionary = data;
+                result.prvDict = data;
+            });
+            result.setLang = function(newLang) {
+                for (var i in bflConfig.langs) {
+                    if (bflConfig.langs[i] === newLang) {
+                        result.lang = newLang;
+                        $http.get(bflConfig.baseUrl + result.lang).success(function (data) {
+                            result.prvDict = result.dictionary;
+                            result.dictionary = data;
+
+                        }).error(function() {
+                            result.prvDict = result.dictionary;
+                            result.dictionary = [];
+                        });
+
+                        return;
+                    }
+                }
+            };
+            return result;
+        })
+        .directive('bflTranslate', function(bflLanguage) {
+            return {
+                link : function(scope, element, attrs) {
+                    scope.$watch(function() {
+                        return bflLanguage.dictionary
+                    }, function(dictionary) {
+                        element.text(
+                            dictionary[bflLanguage.prvDict.indexOf(element.text())] ||
+                            bflLanguage.defaultDict[bflLanguage.prvDict.indexOf(element.text())] ||
+                            element.text()
+                        );
+                    })
+                }
+            }
+        });
+
+}(angular));
